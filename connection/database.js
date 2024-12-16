fd202ba3ae0209feca81cb47af01526ed0fc9335
Harikalyan <harikalyan99tronics@gmail.com>
2024-12-16 18:55:18 +0530

    path change for config

--- connection/database.js
+++ connection/database.js
@@ -1,8 +1,4 @@
-const { Sequelize} = require('sequelize');
-const config = require('../config/config')[env];
-
-const env = config.NODE_ENV || 'development';
+const { Sequelize } = require("sequelize");
+const config = require("../config/config")[env];
 const sequelize = new Sequelize(config);
-
-module.exports =  sequelize;
-
+module.exports = sequelize;
