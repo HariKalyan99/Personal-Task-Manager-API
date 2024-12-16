81db4a0691ae6a1de40a0cbf70db5df691b02f8e
Harikalyan <harikalyan99tronics@gmail.com>
2024-12-16 17:57:39 +0530

    as prod

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ const dotenv = require("dotenv");
 dotenv.config({ path: `${process.cwd()}/.env` });
 
 const config = {
-  development: {
+  production: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
