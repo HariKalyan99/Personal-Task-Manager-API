ea751ad299f19e90487654f32b3f0c44f3c3f861
Harikalyan <harikalyan99tronics@gmail.com>
2024-12-17 06:37:28 +0530

    merged the refactored branch removed test

--- config/config.js
+++ config/config.js
@@ -11,15 +11,6 @@ const config = {
     dialect: "postgres",
     seederStorage: "sequelize",
   },
-  test: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "postgres",
-    seederStorage: "sequelize",
-  },
   production: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
