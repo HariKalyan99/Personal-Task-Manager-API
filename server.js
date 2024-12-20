a36f57e05ffacdbe33dc8567ef44f2430af0f34d
Harikalyan <harikalyan99tronics@gmail.com>
2024-12-20 18:41:50 +0530

    allowed cors

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require('cors');
 const authRouter = require("./route/auth.route");
 const taskRouter = require("./route/task.route");
 const catchAsync = require("./middlewares/catchAsync");
@@ -10,8 +11,10 @@ const config = require("./config/config");
 
 
 
+
 const app = express();
 const PORT = config.APP_PORT;
+app.use(cors());
 app.use(express.json());
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/", taskRouter);
