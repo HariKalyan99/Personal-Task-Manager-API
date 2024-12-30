bbfc9b8aac91548a0967aa6fd248a2830429f52b
Harikalyan99 <harikalyan99tronics@gmail.com>
2024-12-30 17:37:54 +0530

    added the cors

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
-const express = require('express');
-const cors = require('cors');
-const authRouter = require('./route/auth.route');
-const taskRouter = require('./route/task.route');
-const globalErrorHandler = require('./controller/error.controller');
-const sequelize = require('./connection/connectToSQL');
-const config = require('./config');
+const express = require("express");
+const cors = require("cors");
+const authRouter = require("./route/auth.route");
+const taskRouter = require("./route/task.route");
+const globalErrorHandler = require("./controller/error.controller");
+const sequelize = require("./connection/connectToSQL");
+const config = require("./config");
 
 const app = express();
 const PORT = config.appport;
-// use cors methods
-app.use(cors());
+app.use(cors({
+  origin: 'https://task-manger-fullstack.vercel.app',
+  credentials: true,
+}));
+
 app.use(express.json());
-app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/', taskRouter);
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/", taskRouter);
 
 app.use(
-  '*',
+  "*",
   async (request, response, next) => {
     response
       .status(404)
